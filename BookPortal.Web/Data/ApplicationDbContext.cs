58254ae5c42970cd018fd59cad2eecec90055aa5
tran viet loc <iamtvloc02@gmail.com>
2025-01-20 13:51:39 +0700

    ok

---
+++ BookPortal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookPortal.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookPortal.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ BookPortal.Web/Models/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookPortal.Web.Models.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public string Category { get; set; }
+    }
+}
